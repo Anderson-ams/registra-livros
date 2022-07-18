25bd163f6342e224d61884d151aadc3d561d890c
Anderson Matos <ams.bns2021@gmail.com>
2022-07-18 14:56:08 -0300

    init commit

---
+++ src/main/java/br/com/dev/anderson/registralivro/RegistraLivroApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.anderson.registralivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistraLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistraLivroApplication.class, args);
+	}
+
+}
